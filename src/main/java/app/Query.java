0333c5f711eebe80edd5b1a33721b39cf6ad5fee
Massimiliano Roccamena <massimiliano.roccamena@mail.polimi.it>
2021-01-31 11:24:36 +0100

    update

--- src/main/java/app/Query.java
+++ src/main/java/app/Query.java
@@ -12,7 +12,7 @@ import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.expressions.WindowSpec;
 import org.apache.spark.sql.functions;
 
-public class Query1 
+public class Query 
 {
     private static final String name = "Query3";
     private static final boolean useCache = true;
@@ -29,14 +29,30 @@ public class Query1
                 .appName(name)
                 .getOrCreate();
 
-        int window = 7;
+        //Q1
+        int windowSize = 7;
 
         Dataset<Row> input = spark.read().json("data/data.csv");
-        Dataset<Row> byCountry = input.repartition(input.col("country"));
-        WindowSpec slidingWindow = Window.orderBy("day").rangeBetween(-window, 0);
-        Dataset<Row> output = byCountry.withColumn("MA(7)", functions.avg("cases").over(slidingWindow));
+
+        WindowSpec window = Window.partitionBy("country")
+                                            .orderBy("day")
+                                            .rangeBetween(-windowSize, 0);
+
+        Dataset<Row> output1 = input.withColumn("Q1", functions.avg("cases")
+                                                                .over(window));
         
-        output.show();
+        output1.show();
+
+        //Q2
+        window = Window.partitionBy("country")
+                            .orderBy("day");
+
+        Dataset<Row> output2 = output1.withColumn("Q2",
+                                                    (output1.Q1 - functions.lag(output1.Q1, 1).over(window))/100)
+
+        output2.show();
+
+        //Q3
         
         spark.close();
     }
