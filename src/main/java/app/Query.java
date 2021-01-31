f11160cb887e3604fc68b2f537c779f1ce4f3f03
Massimiliano Roccamena <massimiliano.roccamena@mail.polimi.it>
2021-01-31 16:51:15 +0100

    update

--- src/main/java/app/Query.java
+++ src/main/java/app/Query.java
@@ -3,14 +3,17 @@ package app;
 import java.util.List;
 import java.util.ArrayList;
 
-
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.expressions.WindowSpec;
-import org.apache.spark.sql.functions;
+
+import static org.apache.spark.sql.functions.*;
 
 public class Query 
 {
@@ -19,6 +22,7 @@ public class Query
 
     public static void main( String[] args )
     {
+        // Setup
         final String master = args.length > 0 ? args[0] : "local[4]";
         final String filePath = args.length > 1 ? args[1] : "./";
         final String appName = useCache ? name+"WithCache" : name+"NoCache";
@@ -29,31 +33,53 @@ public class Query
                 .appName(name)
                 .getOrCreate();
 
-        //Q1
-        int windowSize = 7;
+        final List<StructField> schemaFields = new ArrayList<StructField>();
+        schemaFields.add(DataTypes.createStructField("country", DataTypes.StringType, true));
+        schemaFields.add(DataTypes.createStructField("day", DataTypes.IntegerType, true));
+        schemaFields.add(DataTypes.createStructField("cases", DataTypes.IntegerType, true));
+        final StructType schema = DataTypes.createStructType(schemaFields);
 
-        Dataset<Row> input = spark.read().json("data/data.csv");
+        Dataset<Row> df = spark.read()
+                                    .option("header", "false")
+                                    .option("delimiter", ",")
+                                    .schema(schema)
+                                    .load("data/test.csv");
 
+        // Q1
         WindowSpec window = Window.partitionBy("country")
                                             .orderBy("day")
-                                            .rangeBetween(-windowSize, 0);
+                                            .rangeBetween(-7, 0);
 
-        Dataset<Row> output1 = input.withColumn("Q1", functions.avg("cases")
-                                                                .over(window));
+        df = df.withColumn("Q1", 
+                                                avg("cases").over(window));
         
-        output1.show();
+        df.persist();
+        df.show();
 
-        //Q2
+        // Q2
         window = Window.partitionBy("country")
-                            .orderBy("day");
+                        .orderBy("day");
 
-        Dataset<Row> output2 = output1.withColumn("Q2",
-                                                    (output1.Q1 - functions.lag(output1.Q1, 1).over(window))/100)
+        df = df.withColumn("lagged",
+                                     lag(df.col("Q1"), 1).over(window));
 
-        output2.show();
+        df = df.withColumn("Q2",
+                                    expr("cases/lagged").over(window));
 
-        //Q3
+        df = df.drop("lagged");
+
+        df.persist();
+        df.show();
+
+        // Q3
+
+        df.groupBy("country").agg(sum("Q2"));
+
+        df = df.orderBy(desc("sum(Q2)")).limit(10).select("country");
+
+        df.show();
         
+        // Close
         spark.close();
     }
 }
