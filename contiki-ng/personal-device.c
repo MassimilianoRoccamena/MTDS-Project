bf5ecda900dd1275ced9c09c6a82af0b10825402
MassimilianoRoccamena <massimiliano.roccamena@mail.polimi.it>
2021-03-23 12:13:17 +0100

    cleaning

--- contiki-ng/personal-device.c
+++ contiki-ng/personal-device.c
@@ -10,58 +10,54 @@
 #include <string.h>
 
 #include "sys/log.h"
-#define LOG_MODULE "App"
-#define LOG_LEVEL LOG_LEVEL_INFO
+#define LOG_MODULE 		"App"
+#define LOG_LEVEL 		LOG_LEVEL_INFO
 
-#define UDP_PORT 1234
+#define UDP_PORT 		1234
+#define CONTACTS_INTERVAL	(5 * CLOCK_SECOND)
+#define VERBOSE_LEVEL		1
 
-#define SEND_INTERVAL		(5 * CLOCK_SECOND)
-#define SEND_TIME		(random_rand() % (SEND_INTERVAL))
-
-static struct simple_udp_connection broadcast_connection;
+static struct simple_udp_connection contacts_connection;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(contacts_process, "Contacts detection process");
 AUTOSTART_PROCESSES(&contacts_process);
 /*---------------------------------------------------------------------------*/
-static void
-receiver(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
+static void contacts_receiver(struct simple_udp_connection *c,
+         			const uip_ipaddr_t *sender_addr,
+         			uint16_t sender_port,
+         			const uip_ipaddr_t *receiver_addr,
+         			uint16_t receiver_port,
+         			const uint8_t *data,
+         			uint16_t datalen)
 {
-  LOG_INFO("Detected contact with ");
-  LOG_INFO_6ADDR(sender_addr);
-  LOG_INFO_("\n");
+  if (VERBOSE_LEVEL > 0) {
+    LOG_INFO("Detected contact with ");
+    LOG_INFO_6ADDR(sender_addr);
+    LOG_INFO_("\n");
+  }
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(contacts_process, ev, data)
 {
-  static struct etimer periodic_timer;
-  static struct etimer send_timer;
+  static struct etimer contacts_timer;
+
   uip_ipaddr_t addr;
   uip_create_linklocal_allnodes_mcast(&addr);
 
   PROCESS_BEGIN();
+  
+  simple_udp_register(&contacts_connection, UDP_PORT, NULL, UDP_PORT, contacts_receiver);
+  etimer_set(&contacts_timer, CONTACTS_INTERVAL);
 
-  simple_udp_register(&broadcast_connection, UDP_PORT,
-                      NULL, UDP_PORT,
-                      receiver);
-
-  etimer_set(&periodic_timer, SEND_INTERVAL);
   while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-    etimer_reset(&periodic_timer);
-    etimer_set(&send_timer, SEND_TIME);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&contacts_timer));
+    etimer_reset(&contacts_timer);
+    
+    if (VERBOSE_LEVEL > 1)
+      LOG_INFO("Sending broadcast");
 
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&send_timer));
-    LOG_INFO("Sending broadcast to ");
-    LOG_INFO_6ADDR(&addr);
-    LOG_INFO("\n");
-    simple_udp_sendto(&broadcast_connection, "", sizeof(""), &addr);
+    simple_udp_sendto(&contacts_connection, "", sizeof(""), &addr);
   }
 
   PROCESS_END();
